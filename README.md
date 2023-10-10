bd5284e384466d3ff8be055efb98e02756c4d8c6
adityakaduskar <59116309+adityakaduskar@users.noreply.github.com>
2023-10-10 14:04:32 -0400

    Added Requirements

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Disease-Classification-MLFlow-DVC
+
+# To run this project
+### STEPS:
+Clone the repository
+
+```bash
+https://github.com/adityakaduskar/Disease-Classification-MLFlow-DVC
+```
+
+### STEP 1 - Create a conda environment after cloning the repository
+
+```bash
+conda create -n cnncls python=3.8 -y
+```
+
+```bash
+conda activate cnncls
+```
+
+### STEP 2 - Install the requirements
+```bash
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+    
+__version__ = "0.0.0"
+REPO_NAME = "Disease-Classification-MLFlow-DVC"
+AUTHOR_USER_NAME = "adityakaduskar"
+AUTHOR_EMAIL = "adityakaduskar@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version = __version__,
+    author = AUTHOR_USER_NAME,
+    author_email = AUTHOR_EMAIL,
+    description = "A small Python Package for a CNN app",
+    Long_description = long_description
+    Long_description_content = "text/markdown",
+    url = f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}"
+    
+)
