d76127759d6a1462c72db5810f19c7a968fd00cc
adityakaduskar <59116309+adityakaduskar@users.noreply.github.com>
2023-10-10 18:50:27 -0400

    Added main.py and logger

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 __version__ = "0.0.0"
 REPO_NAME = "Disease-Classification-MLFlow-DVC"
 AUTHOR_USER_NAME = "adityakaduskar"
+SRC_REPO = "cnnClassifier"
 AUTHOR_EMAIL = "adityakaduskar@gmail.com"
 
 setuptools.setup(
@@ -14,8 +15,8 @@ setuptools.setup(
     author = AUTHOR_USER_NAME,
     author_email = AUTHOR_EMAIL,
     description = "A small Python Package for a CNN app",
-    Long_description = long_description
+    Long_description = long_description,
     Long_description_content = "text/markdown",
-    url = f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}"
-    
+    url = f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls = {"Bug Tracker" : f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues"},
 )
